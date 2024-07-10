578c50adce92457be198abdbb89d1568e9c638d6
Jack <jack20220723@gmail.com>
2024-07-10 08:54:21 +0800

    Decoupling acceptance tests from the real AWS account

--- provisioner/sonatype-nexus-repository/provisioner_acc_test.go
+++ provisioner/sonatype-nexus-repository/provisioner_acc_test.go
@@ -15,56 +15,10 @@ import (
 	"github.com/hashicorp/packer-plugin-sdk/acctest"
 )
 
-//go:embed test-fixtures/template-aws.pkr.hcl
-var testProvisionerHCL2AWS string
-
 //go:embed test-fixtures/template-docker.pkr.hcl
 var testProvisionerHCL2Docker string
 
 func TestAccSonatypeNexusRepositoryProvisioner(t *testing.T) {
-	testCaseAws := &acctest.PluginTestCase{
-		Name: "sonatype_nexus_repository_provisioner_aws_test",
-		Setup: func() error {
-			return nil
-		},
-		Teardown: func() error {
-			return nil
-		},
-		Template: testProvisionerHCL2AWS,
-		Type:     "hashicorp-aws-sonatype-nexus-repository-provisioner",
-		Check: func(buildCommand *exec.Cmd, logfile string) error {
-			if buildCommand.ProcessState != nil {
-				if buildCommand.ProcessState.ExitCode() != 0 {
-					return fmt.Errorf("Bad exit code. Logfile: %s", logfile)
-				}
-			}
-
-			logs, err := os.Open(logfile)
-			if err != nil {
-				return fmt.Errorf("Unable find %s", logfile)
-			}
-			defer logs.Close()
-
-			logsBytes, err := ioutil.ReadAll(logs)
-			if err != nil {
-				return fmt.Errorf("Unable to read %s", logfile)
-			}
-			logsString := string(logsBytes)
-
-			errorString := "\\[ERROR\\] Remote command exited with"
-			if matched, _ := regexp.MatchString(".*"+errorString+".*", logsString); matched {
-				t.Fatalf("%s\n Acceptance tests for %s failed. Please search for '%s' in log file at %s", logsString, "sonatype-nexus-repository provisioner", errorString, logfile)
-			}
-
-			provisionerOutputLog := "amazon-ebs.hashicorp-aws: AMIs were created:"
-			if matched, _ := regexp.MatchString(provisionerOutputLog+".*", logsString); !matched {
-				t.Fatalf("%s\n logs doesn't contain expected output %q", logsString, provisionerOutputLog)
-			}
-			return nil
-		},
-	}
-	acctest.TestPlugin(t, testCaseAws)
-
 	testCaseDocker := &acctest.PluginTestCase{
 		Name: "sonatype_nexus_repository_provisioner_docker_test",
 		Setup: func() error {
